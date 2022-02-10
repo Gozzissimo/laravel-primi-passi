e1b802c3d722e2e94f6b9eae72708730fe94814b
Gozzissimo <fantini.nico@gmail.com>
2022-02-10 17:41:04 +0100

    test2

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Bikes | Road</title>
+    <title>Laravel Bikes | TEST</title>
 </head>
 <body>
-    <h1>Road</h1>
+    <h1>TEST</h1>
 </body>
 </html>
